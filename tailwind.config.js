c7269f5c1a009070ba5b25ba1811523e5b6b94fb
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2024-02-09 10:03:50 -0500

    Changes Initial

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./Index.html",
+    "./Src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
