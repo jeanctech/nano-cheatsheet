6fd6f3c535f78823fbe1acde2e23e3fac9ae0f94
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2024-08-06 07:02:47 -0500

    Changes - Update

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
 import react from "../assets/react.svg";
 import vite from "/vite.svg";
-import '../styles/app.css';
+import App_Header from "./app_header";
+import App_Card from "./app_card";
+import App_Canales from "./app_canales";
+import "../styles/app.css";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <>
-      <div id="app"
-        className="m-5 p-5 flex flex-col justify-center items-center" >
-        <div id="app-container"
-          className="m-5 p-5 flex flex-row justify-start items-center" >
-          <h1 className="m-2.5 p-2.5 text-2.5xl font-semibold" >
-            Pages-Tv
-          </h1>
+      <div
+        id="app"
+        className="m-5 p-5 flex flex-col justify-center items-center"
+      >
+        <div
+          id="app-container"
+          className="m-5 p-5 flex flex-row justify-start items-center"
+        >
+          <h1 className="m-2.5 p-2.5 text-2.5xl font-semibold">Pages-Tv</h1>
           <a href="https://vitejs.dev">
             <img src={vite} className="m-1 p-1" alt="Vite-Logo" width={75} />
           </a>
@@ -21,16 +26,21 @@ function App() {
             <img src={react} className="m-1 p-1" alt="React-Logo" width={75} />
           </a>
         </div>
-        <div id="app-card"
-          className="m-2.5 p-2.5 flex flex-row justify-center items-center" >
-          <button onClick={() => setCount((count) => count + 1)} >
+        <App_Header />
+        <App_Card />
+        <App_Canales />
+        <div
+          id="app-card"
+          className="m-2.5 p-2.5 flex flex-row justify-center items-center"
+        >
+          <button onClick={() => setCount((count) => count + 1)}>
             Count Is - {count}
           </button>
-          <p className="m-2.5 p-2.5 bg-[#1A1A21] text-slate-300 rounded-xl" >
+          <p className="m-2.5 p-2.5 bg-[#1A1A21] text-slate-300 rounded-xl">
             Edit <code>Src/App.jsx</code> and save to test Hmr
           </p>
         </div>
-        <p id="read" className="m-2.5 p-2.5 bg-[#1A121A] rounded-lg"  >
+        <p id="read" className="m-2.5 p-2.5 bg-[#1A121A] rounded-lg">
           Click on the Vite and React Logos to learn more
         </p>
       </div>
---
+++ src/components/app_canales.jsx
@@ -0,0 +1,86 @@
+import Image_One from "../assets/1.png"
+import Image_Two from "../assets/2.png"
+import "../styles/app.css";
+
+function App_Canales() {
+  return (
+    <>
+      <div
+        className="m-0 p-5 flex flex-col text-[#b8adad] justify-center items-center rounded-lg"
+      >
+        <div className="m-0 p-1 bg-[#1f1d1d] flex flex-row flex-wrap rounded-lg">
+          <div className="m-1 p-1 flex flex-col">
+          <a href="" className="m-1 p-1">
+            <img src={Image_One} className="m-1 p-1 rounded-lg" alt="Rcn-Logo" width={75} />
+          </a>
+          <span className="m-1 p-1">Canal Rcn</span>
+          </div>
+          <div className="m-1 p-1 flex flex-col">
+          <a href="" className="m-1 p-1">
+            <img src={Image_Two} className="m-1 p-1 rounded-lg" alt="Caracol-Logo" width={75} />
+          </a>
+          <span className="m-1 p-1">Canal Caracol</span>
+          </div>
+          <div className="m-1 p-1 flex flex-col">
+          <img src={Image_One} className="m-1 p-1 rounded-lg" alt="Rcn-Logo" width={75} />
+          <span className="m-1 p-1">Canal Rcn</span>
+          </div>
+          <div className="m-1 p-1 flex flex-col">
+          <img src={Image_One} className="m-1 p-1 rounded-lg" alt="Rcn-Logo" width={75} />
+          <span className="m-1 p-1">Canal Rcn</span>
+          </div>
+          <div className="m-1 p-1 flex flex-col">
+          <img src={Image_One} className="m-1 p-1 rounded-lg" alt="Rcn-Logo" width={75} />
+          <span className="m-1 p-1">Canal Rcn</span>
+          </div>
+        </div>
+      <div className="m-0 p-1 bg-[#1f1d1d]  flex flex-row flex-wrap rounded-lg">
+          <div className="m-1 p-1 flex flex-col">
+          <img src={Image_One} className="m-1 p-1 rounded-lg" alt="Rcn-Logo" width={75} />
+          <span className="m-1 p-1">Canal Rcn</span>
+          </div>
+          <div className="m-1 p-1 flex flex-col">
+          <img src={Image_One} className="m-1 p-1 rounded-lg" alt="Rcn-Logo" width={75} />
+          <span className="m-1 p-1">Canal Rcn</span>
+          </div>
+          <div className="m-1 p-1 flex flex-col">
+          <img src={Image_One} className="m-1 p-1 rounded-lg" alt="Rcn-Logo" width={75} />
+          <span className="m-1 p-1">Canal Rcn</span>
+          </div>
+          <div className="m-1 p-1 flex flex-col">
+          <img src={Image_One} className="m-1 p-1 rounded-lg" alt="Rcn-Logo" width={75} />
+          <span className="m-1 p-1">Canal Rcn</span>
+          </div>
+          <div className="m-1 p-1 flex flex-col">
+          <img src={Image_One} className="m-1 p-1 rounded-lg" alt="Rcn-Logo" width={75} />
+          <span className="m-1 p-1">Canal Rcn</span>
+          </div>
+        </div>
+        <div className="m-0 p-1 bg-[#1f1d1d] flex flex-row flex-wrap rounded-lg">
+          <div className="m-1 p-1 flex flex-col">
+          <img src={Image_One} className="m-1 p-1 rounded-lg" alt="Rcn-Logo" width={75} />
+          <span className="m-1 p-1">Canal Rcn</span>
+          </div>
+          <div className="m-1 p-1 flex flex-col">
+          <img src={Image_One} className="m-1 p-1 rounded-lg" alt="Rcn-Logo" width={75} />
+          <span className="m-1 p-1">Canal Rcn</span>
+          </div>
+          <div className="m-1 p-1 flex flex-col">
+          <img src={Image_One} className="m-1 p-1 rounded-lg" alt="Rcn-Logo" width={75} />
+          <span className="m-1 p-1">Canal Rcn</span>
+          </div>
+          <div className="m-1 p-1 flex flex-col">
+          <img src={Image_One} className="m-1 p-1 rounded-lg" alt="Rcn-Logo" width={75} />
+          <span className="m-1 p-1">Canal Rcn</span>
+          </div>
+          <div className="m-1 p-1 flex flex-col">
+          <img src={Image_One} className="m-1 p-1 rounded-lg" alt="Rcn-Logo" width={75} />
+          <span className="m-1 p-1">Canal Rcn</span>
+          </div>
+        </div>
+    </div>
+    </>
+  );
+}
+
+export default App_Canales;
